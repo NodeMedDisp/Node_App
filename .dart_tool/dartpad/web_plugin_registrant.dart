74717cf64766e4ef72c93b79378757eaed64b223
vizjonny <vizmegjonny@gmail.com>
2024-09-24 18:12:50 -0700

    resolved conflicts and commit

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,13 +6,21 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:network_info_plus/src/network_info_plus_web.dart';
+import 'package:rive_common/rive_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  ConnectivityPlusWebPlugin.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  NetworkInfoPlusWebPlugin.registerWith(registrar);
+  RivePlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
